ef6c38baabf9adc77464f0b8cdf0384e4df83d77
Maurice Ainon <maurice.ainon@capgemini.com>
2022-01-05 02:20:56 +0000

    Finish omdb api

---
+++ omdb/OmdbClient.py
@@ -0,0 +1,41 @@
+class OmdbClient:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def make_request(self, params):
+        """Make a GET request to the API, automatically adding the `apikey` to parameters."""
+        params["apikey"] = self.api_key
+
+        resp = requests.get(OMDB_API_URL, params=params)
+        resp.raise_for_status()
+        return resp
+
+    def get_by_imdb_id(self, imdb_id):
+        """Get a movie by its IMDB ID"""
+        logger.info("Fetching detail for IMDB ID %s", imdb_id)
+        resp = self.make_request({"i": imdb_id})
+        return OmdbMovie(resp.json())
+
+    def search(self, search):
+        """Search for movies by title. This is a generator so all results from all pages will be iterated across."""
+        page = 1
+        seen_results = 0
+        total_results = None
+
+        logger.info("Performing a search for '%s'", search)
+
+        while True:
+            logger.info("Fetching page %d", page)
+            resp = self.make_request({"s": search, "type": "movie", "page": str(page)})
+            resp_body = resp.json()
+            if total_results is None:
+                total_results = int(resp_body["totalResults"])
+
+            for movie in resp_body["Search"]:
+                seen_results += 1
+                yield OmdbMovie(movie)
+
+            if seen_results >= total_results:
+                break
+
+            page += 1
