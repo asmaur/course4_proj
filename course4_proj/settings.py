e3fd658fe833e177ceb96b18140e12a2f53a99b6
Maurice Ainon <maurice.ainon@capgemini.com>
2022-01-06 16:19:31 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -42,6 +42,8 @@ class Dev(Configuration):
     OMDB_KEY = "abc123"
     CELERY_RESULT_BACKEND = "django-db"
     CELERY_BROKER_URL = "redis://localhost:6379/0"
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ADMINS = [("Ben", "ben@example.com")]
 
 
     # Application definition
